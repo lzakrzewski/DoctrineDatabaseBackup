d209542ae63258e6c35e052c4e7332d2aab20429
lucaszz <lucaszz111@gmail.com>
2015-10-05 13:47:27 +0200

    Command introduced

---
+++ tests/Backup/CommandTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Lucaszz\DoctrineDatabaseBackup\tests\Backup;
+
+use Lucaszz\DoctrineDatabaseBackup\Backup\Command;
+
+class CommandTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Command */
+    private $command;
+
+    /** @test */
+    public function it_returns_output_of_command_was_executed_with_success()
+    {
+        $this->assertEquals("success\n", $this->command->run("echo 'success'"));
+    }
+
+    /**
+     * @test
+     * @expectedException \RuntimeException
+     */
+    public function it_fails_when_command_failed()
+    {
+        $this->command->run('/bin/false');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->command = new Command();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown()
+    {
+        $this->command = null;
+    }
+}
