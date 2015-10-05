1da5c2bca10d9c1031780798c4488590d747bfc5
lucaszz <lucaszz111@gmail.com>
2015-10-05 20:02:30 +0200

    Example usage

---
+++ tests/Integration/ExampleTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Lucaszz\DoctrineDatabaseBackup\tests\Backup\Executor;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\ORM\Tools\Setup;
+use Lucaszz\DoctrineDatabaseBackup\Backup\DoctrineDatabaseBackup;
+use Lucaszz\DoctrineDatabaseBackup\tests\Integration\Entity\TestProduct;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var EntityManager */
+    private $entityManager;
+
+    public function testThatDatabaseContainsProduct()
+    {
+        $this->entityManager->persist(new TestProduct('Teapot', 25));
+        $this->entityManager->flush();
+
+        $this->assertCount(1, $this->entityManager->getRepository('\Lucaszz\DoctrineDatabaseBackup\tests\Integration\Entity\TestProduct')->findAll());
+    }
+
+    public function testThatDatabaseIsClearBeforeNextTest()
+    {
+        $this->entityManager->persist(new TestProduct('Iron', 99));
+        $this->entityManager->flush();
+
+        $this->assertCount(1, $this->entityManager->getRepository('\Lucaszz\DoctrineDatabaseBackup\tests\Integration\Entity\TestProduct')->findAll());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->entityManager = $this->createEntityManager();
+
+        $backup = new DoctrineDatabaseBackup($this->entityManager);
+
+        if (!$backup->isCreated()) {
+            $backup->clearDatabase();
+            $backup->create();
+        }
+
+        $backup->restore();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown()
+    {
+        $this->entityManager = null;
+    }
+
+    /**
+     * Before first test of PHP process database should be created.
+     */
+    public static function setUpBeforeClass()
+    {
+        $entityManager = self::createEntityManager();
+        self::setupDatabase($entityManager);
+    }
+
+    /**
+     * Example of creating EntityManager.
+     */
+    private static function createEntityManager()
+    {
+        $entityPath = array(__DIR__.'/Entity');
+
+        $config = Setup::createAnnotationMetadataConfiguration($entityPath, false);
+        $driver = new AnnotationDriver(new AnnotationReader(), $entityPath);
+        AnnotationRegistry::registerLoader('class_exists');
+        $config->setMetadataDriverImpl($driver);
+
+        return EntityManager::create(self::getParams(), $config);
+    }
+
+    /**
+     * Example of setup database before test.
+     */
+    private function setupDatabase(EntityManager $entityManager)
+    {
+        $params = self::getParams();
+
+        $tmpConnection = DriverManager::getConnection($params);
+        $tmpConnection->getSchemaManager()->createDatabase($params['path']);
+
+        $schemaTool = new SchemaTool($entityManager);
+        $schemaTool->dropDatabase();
+
+        $class = 'Lucaszz\DoctrineDatabaseBackup\tests\Integration\Entity\TestProduct';
+
+        $schemaTool->createSchema(array($entityManager->getClassMetadata($class)));
+    }
+
+    /**
+     * Example of Database connection parameters.
+     */
+    private static function getParams()
+    {
+        return array(
+            'driver' => 'pdo_sqlite',
+            'user' => 'root',
+            'password' => '',
+            'path' => __DIR__.'/database/sqlite.db',
+        );
+    }
+}
